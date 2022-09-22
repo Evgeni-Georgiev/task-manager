e6f6c6ccb834e9bc1cd91bc5c9c3869ff1b573d4
Evgeni Georgiev <evgenigg2000@gmail.com>
2022-09-22 23:25:58 +0300

    Signed as user's name span added.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,8 +3,11 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import {getLoggedUser} from "../../utils/http-utils/user-requests";
 
 export function Header() {
+
+    const currentLogged = getLoggedUser();
     return (
         <div className="header">
             <Navbar bg="light" expand="lg">
@@ -14,9 +17,16 @@ export function Header() {
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
                             <Link className="nav-link" to="/users-list">Users List</Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
+                            <Link className="nav-link" to="/user/create-user">Create User</Link>
                         </Nav>
                     </Navbar.Collapse>
+
+                    <Navbar.Collapse className="justify-content-end">
+                        <Navbar.Text>
+                            Signed in as: <span>{currentLogged.name}</span>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+
                 </Container>
             </Navbar>
         </div>
