da720622d5e83ca7594711c16cda98b1e1aae8ab
Evgeni Georgiev <evgenigg2000@gmail.com>
2022-09-22 23:28:16 +0300

    Routes with certain access to the application are added to the App.js. Only logged users can now access the Layout.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import { Layout } from './components/layout/Layout';
+import { Route, Routes } from "react-router-dom";
+import {Register} from "./components/auth/register/Register";
+import {UsersList} from "./components/users/users-list/UsersList";
+import {User} from "./components/users/user/User";
+import {UserForm} from "./components/users/user-form/UserForm";
+import {Login} from "./components/auth/login/Login";
+import {AuthenticatedRoute} from "./utils/guards/AuthenticatedRoute";
+import {NonAuthenticatedGuard} from "./utils/guards/NonAuthenticatedGuard";
 
 function App() {
   return (
     <div className="App">
-      <Layout />
+        <Routes>
+            <Route exact path="/register" element={<NonAuthenticatedGuard><Register/></NonAuthenticatedGuard>} />
+            <Route exact path="/login" element={<NonAuthenticatedGuard><Login/></NonAuthenticatedGuard>} />
+
+            <Route exact path="/" element={<AuthenticatedRoute><Layout/></AuthenticatedRoute>} >
+                <Route path="/users-list" element={<UsersList/>} />
+                <Route path="/user/:id" element={<User />} />
+                <Route path="/user/create-user" element={<UserForm />} />
+                <Route path="/user/edit/:id" element={<UserForm />} />
+            </Route>
+        </Routes>
     </div>
   );
 }
