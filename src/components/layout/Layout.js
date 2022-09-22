d014792ab80490f10f5657453d47d5e5f7feb815
Evgeni Georgiev <evgenigg2000@gmail.com>
2022-09-22 19:46:24 +0300

    Set Layout component to render Header, Main and Footer components.

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,38 +1,11 @@
-// Components:
-// - Small and reusable
-// - Strongly specialized in for one purpose
-import { useState } from "react";
 import { Header } from "../header/Header"
 import { Footer } from "../footer/Footer"
-import { Main } from "../main/Main"
-import { useRef } from 'react';
+import { Main } from "../main/Main";
 export function Layout() {
-
-    let btnRef = useRef();
-    const [message, setMessage] = useState("Layout state");
-    const [count, setCount] = useState(0);
-    const [intervalID, setIntervalID] = useState();
-
-    const counterEve = (e) => {
-        if (btnRef.current) {
-            btnRef.current.setAttribute("disabled", "disabled");
-        }
-        const int = setInterval(() => { setCount((oldCount) => oldCount + 1) }, 500)
-        // setCount(count + 1)
-        setIntervalID(int);
-    }
-
-    const stopInterval = () => {
-        clearInterval(intervalID)
-        setIntervalID(null)
-    }
-
     return (
         <div>
             <Header />
-            Layout
-            <Main message={message} counter={count} stopInterval={stopInterval} />
-            <button ref={btnRef} onClick={counterEve}>Click</button>
+            <Main/>
             <Footer />
         </div>
     );
