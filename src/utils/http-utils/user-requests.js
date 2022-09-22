5dd670af188b3743ed26ae61329a6f4e93d604ee
Evgeni Georgiev <evgenigg2000@gmail.com>
2022-09-22 20:02:29 +0300

    Using Axios to make requests to the json-server.

---
+++ src/utils/http-utils/user-requests.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const apiUrl = 'http://localhost:4004/users';
+
+export function getAllUsers() {
+    return axios.get(apiUrl);
+}
+
+export function getUserById(id) {
+    return axios.get(`${apiUrl}/${id}`);
+}
+
+export function deleteUser(id) {
+    return axios.delete(`${apiUrl}/${id}`);
+}
